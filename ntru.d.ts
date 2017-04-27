88e7dd801ff0ccc715d9dbca0ddfbed0197cfb6c
Ryan Lester <hacker@linux.com>
2017-04-26 23:36:59 -0400

    type definition fix

--- ntru.d.ts
+++ ntru.d.ts
@@ -1,24 +1,26 @@
-export interface INTRU {
-	/** Maximum plaintext length. */
-	plaintextBytes: number;
+declare module 'ntru' {
+	interface INTRU {
+		/** Maximum plaintext length. */
+		plaintextBytes: number;
 
-	/** Cyphertext length. */
-	cyphertextBytes: number;
+		/** Cyphertext length. */
+		cyphertextBytes: number;
 
-	/** Private key length. */
-	privateKeyBytes: number;
+		/** Private key length. */
+		privateKeyBytes: number;
 
-	/** Public key length. */
-	publicKeyBytes: number;
+		/** Public key length. */
+		publicKeyBytes: number;
 
-	/** Decrypts cyphertext with privateKey. */
-	decrypt (encrypted: Uint8Array|string, privateKey: Uint8Array) : Uint8Array;
+		/** Decrypts cyphertext with privateKey. */
+		decrypt (encrypted: Uint8Array|string, privateKey: Uint8Array) : Uint8Array;
 
-	/** Encrypts plaintext with publicKey. */
-	encrypt (message: Uint8Array|string, publicKey: Uint8Array) : Uint8Array;
+		/** Encrypts plaintext with publicKey. */
+		encrypt (message: Uint8Array|string, publicKey: Uint8Array) : Uint8Array;
 
-	/** Generates key pair. */
-	keyPair () : {privateKey: Uint8Array; publicKey: Uint8Array};
-};
+		/** Generates key pair. */
+		keyPair () : {privateKey: Uint8Array; publicKey: Uint8Array};
+	}
 
-export const ntru: INTRU;
+	const ntru: INTRU;
+}
