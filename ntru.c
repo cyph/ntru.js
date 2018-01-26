6f2ebb8e0ad454866a3d04c69ab114d76f040287
tearsofphoenix <tearsofphoenix@icloud.com>
2018-01-26 14:47:42 +0800

    fix drbg instantiate

--- ntru.c
+++ ntru.c
@@ -9,14 +9,48 @@ uint16_t private_key_len;
 uint16_t cyphertext_len;
 uint16_t plaintext_len;
 
-uint8_t *user_seed = NULL;
+static uint8_t *user_seed = NULL;
 uint32_t dbrg_randombytes (uint8_t *out, uint32_t num_bytes) {
-    if (user_seed) {
-        memcpy(out, user_seed, num_bytes);
-    } else {
-	    randombytes_buf(out, num_bytes);
-	}
+    randombytes_buf(out, num_bytes);
 	DRBG_RET(DRBG_OK);
+}
+
+static uint8_t
+get_entropy(
+    ENTROPY_CMD  cmd,
+    uint8_t     *out)
+{
+    /* 2k/8 bytes of entropy are needed to instantiate a DRBG with a
+     * security strength of k bits. Here k = KLENNGTH.
+     */
+    static size_t kMaxLength = KLENNGTH * 2 / 8;
+    static size_t index;
+
+    if (cmd == INIT) {
+        /* Any initialization for a real entropy source goes here. */
+        index = 0;
+        return 1;
+    }
+
+    if (out == NULL)
+        return 0;
+
+    if (cmd == GET_NUM_BYTES_PER_BYTE_OF_ENTROPY) {
+        /* Here we return the number of bytes needed from the entropy
+         * source to obtain 8 bits of entropy.  Maximum is 8.
+         */
+        *out = 1;                       /* this is a perfectly random source */
+        return 1;
+    }
+
+    if (cmd == GET_BYTE_OF_ENTROPY) {
+        if (index == kMaxLength)
+            return 0;                   /* used up all our entropy */
+
+        *out = user_seed[index++];           /* deliver an entropy byte */
+        return 1;
+    }
+    return 0;
 }
 
 void dispose() {
@@ -26,10 +60,14 @@ void dispose() {
 void ntrujs_init (uint8_t *seed) {
     user_seed = seed;
 	randombytes_stir();
-	ntru_crypto_drbg_external_instantiate(
-		(RANDOM_BYTES_FN) &dbrg_randombytes,
-		&drbg
-	);
+
+	ntru_crypto_drbg_instantiate(KLENNGTH, NULL,
+	    0, (ENTROPY_FN) &get_entropy, &drbg);
+
+//	ntru_crypto_drbg_external_instantiate(
+//		(RANDOM_BYTES_FN) &dbrg_randombytes,
+//		&drbg
+//	);
 
 	NTRU_ENCRYPT_PARAM_SET* params_data	=
 		ntru_encrypt_get_params_with_id(PARAMS)
